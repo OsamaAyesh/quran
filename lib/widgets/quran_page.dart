a5eca9d8b89768207bd3cc8f679e4df325e4681e
Osama Ayesh <mr.ayesh88@gmail.com>
2023-10-02 07:50:43 -0700

    Initial commit

---
+++ lib/widgets/quran_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:quran/core/app_asset.dart';
+
+
+
+class QuranPage extends StatelessWidget {
+  const QuranPage({Key? key, required this.pageIndex}) : super(key: key);
+
+  final int pageIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "${AppAsset.pageBasic}${pageIndex + 1}.png",
+      fit: BoxFit.fill,
+    );
+  }
+}
