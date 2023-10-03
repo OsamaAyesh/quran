18307b41af65c7c28193df2cfd49c0d47df14c2e
Osama Ayesh <mr.ayesh88@gmail.com>
2023-10-03 01:37:03 -0700

    first commit

--- lib/core/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorsC{
-  ColorsC._();
-  static const Color blackColor=Colors.black;
-  static const Color whiteColor=Colors.white;
-  static const Color brownColor=Colors.brown;
-  static const Color secondaryColor=Color(0XFFDFDCCD);
-}
--- lib/core/index.dart
+++
@@ -1,4 +0,0 @@
-
-export 'app_asset.dart';
-export 'app_constant.dart';
-export 'app_extension.dart';
